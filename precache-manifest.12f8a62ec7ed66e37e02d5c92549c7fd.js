c0d56bd59571e003fd3e7516d7749549d71b211d
woland-szr <62295565+woland-szr@users.noreply.github.com>
2020-11-10 15:06:32 +0400

    Updates

---
+++ precache-manifest.12f8a62ec7ed66e37e02d5c92549c7fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd90a5a60c170f27556fa637b7a19d1a",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "4968be547d1aa97c9b14",
+    "url": "/kanban-board/static/css/main.a0a8d681.chunk.css"
+  },
+  {
+    "revision": "fbb30eda1ca5a8f9f6e7",
+    "url": "/kanban-board/static/js/2.e8c3c17a.chunk.js"
+  },
+  {
+    "revision": "ad9f6acf0a6b00f01793fa892036c4ef",
+    "url": "/kanban-board/static/js/2.e8c3c17a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4968be547d1aa97c9b14",
+    "url": "/kanban-board/static/js/main.7de9ccb2.chunk.js"
+  },
+  {
+    "revision": "ba2096fa3b770a19f0f1",
+    "url": "/kanban-board/static/js/runtime-main.8e469a6a.js"
+  }
+]);
